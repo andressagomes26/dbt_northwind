cb2d6364079377f62adb65b06ec154febb1a2498
Andressa Gomes <andressagomesm26@gmail.com>
2024-01-08 16:55:38 +0000

    Models staging

---
+++ models/staging/stg_customers.sql
@@ -0,0 +1,15 @@
+with
+    customers as (
+        select 
+            customer_id
+            , company_name
+            , contact_name
+            , address
+            , city
+            , country
+            , 1 as teste
+        from {{ source('northwind', 'customers') }}
+    )
+
+select *
+from customers
